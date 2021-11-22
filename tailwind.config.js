6af7bc12f079116608752291923f31d9d49ccadf
Michael Nelo <syfers@gumi-os.localdomain>
2021-11-22 12:30:53 -0400

    Added colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@ module.exports = {
   purge: ['./index.html', 'src/**/*/.{vue|jsx|tsx|js|ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: '#4c28b5',
+        'main-hover': '#341492',
+        bgcolor: '#ebf0f3',
+        card: '#ffffff',
+        text: '#202020',
+        subtle: '#6e6e6e'
+      }
+    },
   },
   variants: {
     extend: {},
