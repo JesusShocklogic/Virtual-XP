61ba3fad898024b8083a1c28036e449539ee77f2
Ruth Rivas <rhrivasg@gmail.com>
2021-11-22 15:21:05 -0400

    Add typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,40 @@ module.exports = {
   purge: ['./index.html', 'src/**/*/.{vue|jsx|tsx|js|ts}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+		typography: {
+			'main-title': {
+				css: {
+					fontSize: '32px',
+				},
+			},
+			'button-text': {
+				css: {
+					fontSize: '24px',
+				},
+			},
+			'cat-text': {
+				css: {
+					fontSize: '24px',
+				},
+			},
+			'normal-text': {
+				css: {
+					fontSize: '18px',
+				},
+			},
+			'small-text': {
+				css: {
+					fontSize: '16px',
+				},
+			},
+		}
+	},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
 }
