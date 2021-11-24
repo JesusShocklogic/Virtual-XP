6fb90e724699387a624d000c81c6feb023f747ba
Ruth Rivas <rhrivasg@gmail.com>
2021-11-24 17:02:03 -0400

    Created card component

---
+++ src/components/Card.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <slot></slot>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {},
+})
+export default class Button extends Vue {}
+</script>
+
+<style scoped lang="postcss">
+div {
+  @apply py-5;
+  @apply px-4;
+  @apply h-full;
+  @apply bg-card;
+  @apply shadow-2xl;
+  @apply rounded-xl;
+}
+</style>
