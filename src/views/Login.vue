42dc162cb69ebe999aa30412b04536193959da6c
Michael Nelo <syfers@gumi-os.localdomain>
2021-11-25 16:26:46 -0400

    Created Password Input Component

---
+++ src/views/Login.vue
@@ -0,0 +1,15 @@
+<template></template>
+
+<script lang="ts">
+import Button from "@/components/Button.vue";
+import Input from "@/components/Input.vue";
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  components: {
+    Button,
+    Input,
+  },
+})
+export default class Login extends Vue {}
+</script>
